6439b5fa832e7f91f9207145ad7f2e809d4d04f6
dolluaravindyadav334-dotcom <dolluaravindyadav334@gmail.com>
2026-07-28 15:36:40 +0530

    Tokens

---
+++ Day1.py
@@ -0,0 +1,25 @@
+#print('hlello world')
+
+
+name="DOLLU ARAVIND"
+print(name)
+email_id="dolluaravindyadava@gmail.com"
+
+#pirnt(email_id)#python is case-sensitive -->nameError
+#hefwds
+#True=45
+name = "DOLLU ARAVIND"
+print(name)
+#multi assignment variables
+
+name,location,age="aravind","hyderabad",21
+print(name)
+print(location)
+print(age)
+
+#operators
+
+length= 4
+breadth= 3
+area=length*breadth
+print(area)
