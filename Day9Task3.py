1af67bd497c38ebc8cb0389980161d37be2ed979
dolluaravindyadav334-dotcom <dolluaravindyadav334@gmail.com>
2026-08-06 13:04:35 +0530

    Practice

---
+++ Day9Task3.py
@@ -0,0 +1,39 @@
+'''
+price=list(map(int,input('enter the price:').split(',')))
+sum=0
+for i in price:
+    sum=sum+i
+print(sum)
+
+
+write a python program to analyze the how many upper,lower special char
+password='Arvind@123'
+upper_count =0
+lower_count =0
+digit_count =0
+special_count =0
+for ch in password:
+    if 'A'<=ch<='z'
+    upper+=1
+    elif 'a'<=ch<='z'
+    lower+=1
+    elif '0'<=i<=9:
+        digit+=1
+        else:
+            special+=1
+print(f"upper={upper_count}")
+print(f"lower={lower_count}")
+print(f"digit={digit_count}")
+print(f"special={special_count")
+
+
+#write  a python program to extract the email from @
+email= input("Enter the mail:").split()
+for mail in email:
+    print(mail.split("@")[1])
+'''
+movies=list(map(int,input("Enter a moviename").split(','))
+for movie in Movies:
+
+    
+    
